d531d9d4bd4e317ee19f825f31c959d6331fd7df
Albert Ragány-Németh <me@alb1.hu>
2023-11-23 13:59:30 +0100

    Add more DAP key-bindings

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -3,6 +3,8 @@ local dap = require('dap')
 vim.keymap.set('n', "<C-b>", dap.toggle_breakpoint)
 vim.keymap.set('n', "<F5>", dap.continue)
 vim.keymap.set('n', "<F8>", dap.step_over)
+vim.keymap.set('n', "<F9>", dap.step_into)
+vim.keymap.set('n', "<F10>", dap.step_out)
 
 -- https://github.com/rcarriga/nvim-dap-ui
 require("dapui").setup()
