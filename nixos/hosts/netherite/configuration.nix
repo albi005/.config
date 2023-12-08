775033aee9a66655c528b1e90c7bbcc30eaebc3a
Albert Ragány-Németh <me@alb1.hu>
2023-12-08 23:00:37 +0100

    Add SQL Server docker

--- nixos/hosts/netherite/configuration.nix
+++ nixos/hosts/netherite/configuration.nix
@@ -19,10 +19,36 @@
 
     virtualisation.docker.enable = true;
 
+    users.users.mssql = {
+        isSystemUser = true;
+        group = "mssql";
+    };
+    users.groups.mssql = {
+    };
 
     systemd.targets.sleep.enable = false;
     systemd.targets.suspend.enable = false;
     systemd.targets.hibernate.enable = false;
     systemd.targets.hybrid-sleep.enable = false;
+
+    virtualisation.oci-containers = {
+        backend = "docker";
+        containers = {
+            dfv = {
+                image = "mcr.microsoft.com/mssql/server:2022-latest";
+                volumes = [
+                    "/home/albi/Desktop/dfvdb/data:/var/opt/mssql/data"
+                    "/home/albi/Desktop/dfvdb/log:/var/opt/mssql/log"
+                    "/home/albi/Desktop/dfvdb/secrets:/var/opt/mssql/secrets"
+                ];
+                user = "root";
+                environment = {
+                    ACCEPT_EULA = "Y";
+                    MSSQL_SA_PASSWORD = "<YourStrong!Passw0rd>";
+                };
+                ports = [ "1433:1433" ];
+            };
+        };
+    };
 }
 
