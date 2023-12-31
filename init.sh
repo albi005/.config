af1d8efa630fe2d6c839103e798e24478fd8dac0
Albert Ragány-Németh <me@alb1.hu>
2023-12-31 20:40:35 +0100

    Remove PackerSync from init

--- init.sh
+++ init.sh
@@ -24,5 +24,4 @@ sudo nixos-rebuild switch -I nixos-config=$HOME/.config/nixos/hosts/$HOSTNAME/co
 
 rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-nvim --headless -c ":PackerSync" -c ":q"
 
