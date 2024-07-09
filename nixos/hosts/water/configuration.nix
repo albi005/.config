850479a8a57daa1914070ba0c72f81dc6f2f2c07
Albert Ragány-Németh <me@alb1.hu>
2024-07-09 13:21:19 +0200

    Make function keys default

--- nixos/hosts/water/configuration.nix
+++ nixos/hosts/water/configuration.nix
@@ -11,7 +11,14 @@
     networking.networkmanager.wifi.macAddress = "permanent";
 
     environment.systemPackages = with pkgs; [
+        prismlauncher
     ];
+
+    # use function keys instead of media keys by default
+    # https://wiki.archlinux.org/title/Apple_Keyboard
+    boot.extraModprobeConfig = ''
+        options hid_apple fnmode=2
+    '';
 
     home-manager.users.albi = { ... }: {
         home.file.".config/hypr/host.conf".text = ''
