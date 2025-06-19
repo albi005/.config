5234cb70877d4e029009d149d89d996ee94631ab
Albert Ragány-Németh <me@alb1.hu>
2025-06-19 23:02:51 +0200

    Use redstone to build water

--- nixos/hosts/water/configuration.nix
+++ nixos/hosts/water/configuration.nix
@@ -1,44 +1,60 @@
-{ config, pkgs, lib, ... }:
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-      ../../modules/base.nix
-      ../../modules/desktop.nix
-      ../../modules/dotnet.nix
-    ];
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/base.nix
+    ../../modules/desktop.nix
+    ../../modules/dotnet.nix
+  ];
 
-    networking.hostName = "water"; # Define your hostname.
-    networking.networkmanager.wifi.macAddress = "permanent";
-    # networking.networkmanager.enable = lib.mkForce false;
-    networking.wireless.iwd.enable = false; # no deps wifi daemon
-    # virtualisation.docker.enable = true;
-    # services.postgresql.enable = true;
+  networking.hostName = "water"; # Define your hostname.
+  networking.networkmanager.wifi.macAddress = "permanent";
+  # networking.networkmanager.enable = lib.mkForce false;
+  networking.wireless.iwd.enable = false; # no deps wifi daemon
+  # virtualisation.docker.enable = true;
+  # services.postgresql.enable = true;
 
-    boot.supportedFilesystems = ["apfs"];
+  # nix.settings.trusted-users = ["root" "albi"];
 
-    environment.systemPackages = with pkgs; [
-        impala # tui for iwd
-        uppaal
-        bun
-        jetbrains.idea-ultimate
-        jetbrains.rider
-        # jetbrains.webstorm
-        # prisma
-        # dotnet-sdk_8
-    ];
+  nix.buildMachines = [
+    {
+      hostName = "redstone";
+      sshUser = "albi";
+      systems = ["x86_64-linux"];
+      maxJobs = 8;
+    }
+  ];
 
-    # use function keys instead of media keys by default
-    # https://wiki.archlinux.org/title/Apple_Keyboard
-    boot.extraModprobeConfig = ''
-        options hid_apple fnmode=2
+  nix.settings.substituters = ["ssh://albi@redstone"];
+
+  boot.supportedFilesystems = ["apfs"];
+
+  environment.systemPackages = with pkgs; [
+    impala # tui for iwd
+    uppaal
+    bun
+    jetbrains.idea-ultimate
+    jetbrains.rider
+    # jetbrains.webstorm
+    # prisma
+    # dotnet-sdk_8
+  ];
+
+  # use function keys instead of media keys by default
+  # https://wiki.archlinux.org/title/Apple_Keyboard
+  boot.extraModprobeConfig = ''
+    options hid_apple fnmode=2
+  '';
+
+  home-manager.users.albi = {...}: {
+    home.file.".config/hypr/host.conf".text = ''
+      input {
+          kb_options = caps:swapescape
+      }
     '';
-
-    home-manager.users.albi = { ... }: {
-        home.file.".config/hypr/host.conf".text = ''
-            input {
-                kb_options = caps:swapescape
-            }
-        '';
-    };
+  };
 }
