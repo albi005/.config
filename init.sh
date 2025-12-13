6f019a53bb8aee277cc61d9e85bf5ae285965ffc
Albert Ragány-Németh <me@alb1.hu>
2025-12-13 23:10:41 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash
-#! nix-shell -p bash git neovim
+#! nix-shell -p bash git gum neovim helix nixd nano
 # https://nix.dev/tutorials/first-steps/reproducible-scripts
 
 set -e # exit on error
 
-read -p "Enter the new hostname: " HOSTNAME
+export EDITOR=$(gum choose --header "EDITOR=" "hx" "nvim --clean" "nano")
+export HOSTNAME=$(gum input --prompt "HOSTNAME=" --placeholder "nixos")
 
 rm -fr ~/.config
 git clone --recurse-submodules https://github.com/albi005/.config.git ~/.config
@@ -15,8 +16,9 @@ mkdir -p hosts/$HOSTNAME
 cp /etc/nixos/hardware-configuration.nix hosts/$HOSTNAME
 cp template.nix hosts/$HOSTNAME/configuration.nix
 sed -i "s/HOSTNAME/$HOSTNAME/g" hosts/$HOSTNAME/configuration.nix
-nvim --clean flake.nix hosts/$HOSTNAME/configuration.nix
+
+$EDITOR flake.nix hosts/$HOSTNAME/configuration.nix
+
 git add .
 
-nix --extra-experimental-features "nix-command flakes" flake update ~/.config/nixos
 sudo nixos-rebuild switch --flake ~/.config/nixos
