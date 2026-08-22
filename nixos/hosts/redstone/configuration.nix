d08ce87ba8212c69737ae443bbae4a8f999e47e1
Albert Ragány-Németh <me@alb1.hu>
2026-08-22 15:12:24 +0200

    Revert "Use Rider from unmerged nixpkgs PR"
    This reverts commit 5e826fe55f0548b9eb2eb3eb9807ff70e2138374.

--- nixos/hosts/redstone/configuration.nix
+++ nixos/hosts/redstone/configuration.nix
@@ -1,7 +1,6 @@
 {
   pkgs,
   nixos-unstable,
-  nixpkgs-rider,
   ...
 }:
 {
@@ -117,8 +116,7 @@
     # (pkgs.callPackage ../../pkgs/modelsim { }) # HDL simulator
     # (pkgs.callPackage ../../pkgs/quartus-ii-13_1 { }) # FPGA simulator
     # nixos-unstable.jetbrains.rider
-    # from https://github.com/NixOS/nixpkgs/pull/546636
-    nixpkgs-rider.jetbrains.rider
+    pkgs.jetbrains.rider
     # nixos-unstable.jetbrains.goland
     # krita
     # jetbrains.ruby-mine # pek-next
